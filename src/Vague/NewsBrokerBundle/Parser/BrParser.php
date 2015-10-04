e52836cdbf6b94c95e2a125887f0d6bab24265c3
Dan Haman <haman.dan@drukwerkdeal.nl>
2015-10-04 16:52:33 +0300

    + br parser skeleton

---
+++ src/Vague/NewsBrokerBundle/Parser/BrParser.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: devastator
+ * Date: 04/10/2015
+ * Time: 2:58 PM
+ */
+
+namespace Vague\NewsBrokerBundle\Parser;
+
+
+class BrParser extends BaseParser {
+
+    /**
+     * @param string $source
+     * @return array
+     */
+    public function parse($source)
+    {
+
+    }
+}
