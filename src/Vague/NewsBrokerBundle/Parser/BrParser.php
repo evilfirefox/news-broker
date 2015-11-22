43f81cbc60649ebce51d91c4a768eb4af4eb6b73
Dan Haman <uef_mailbox@yahoo.co.uk>
2015-11-22 13:03:05 +0200

    + br parser complete

--- src/Vague/NewsBrokerBundle/Parser/BrParser.php
+++ src/Vague/NewsBrokerBundle/Parser/BrParser.php
@@ -8,8 +8,19 @@
 
 namespace Vague\NewsBrokerBundle\Parser;
 
+use Vague\NewsBrokerBundle\DataContainer\RssItem;
 
-class BrParser extends BaseParser {
+class BrParser extends BaseParser
+{
+    const REGEX_UTF_SYMBOLS = '/\\u(\w+)/i';
+    const REPLACEMENT = '&#$1;';
+    const INDEX_CONTENT = 'bodytext';
+    const INDEX_EVENT_DATE = 'date';
+    const INDEX_TITLE = 'title';
+    const INDEX_URI = 'pagepath';
+    const XPATH_DESCRIPTION = '//div[@class="teaserContainer"]/div[@class="bottomPart"]/p';
+    const REGEX_DESCRIPTION = '/\<p\s+class=\"[^\"]+\"\s*\>([\w\s\&;,.-]+)\<\/p\>/i';
+    const URL_BASE = 'http://www.br-automation.com';
 
     /**
      * @param string $source
@@ -17,6 +28,29 @@ class BrParser extends BaseParser {
      */
     public function parse($source)
     {
+        $data = json_decode($source, true);
+        $result = array();
+        foreach ($data['records'] as $record) {
+            $container = new RssItem();
+            $container->setTitle($record[static::INDEX_TITLE]);
+            $container->setEventDate(new \DateTime($record[static::INDEX_EVENT_DATE]));
+            $container->setDescription($this->parseDescription($record[static::INDEX_CONTENT]));
+            $url = $this->prepareUrl($record[static::INDEX_URI]);
+            $container->setUrl($url);
+            $container->setGUID($url);
+            $result[] = $container;
+        }
+        return $result;
+    }
 
+    protected function parseDescription($description)
+    {
+        $this->_initialize($description);
+        return $this->_xpath->query(static::XPATH_DESCRIPTION)->item(0)->nodeValue;
+    }
+
+    protected function prepareUrl($uri)
+    {
+        return sprintf(static::MASK_FULL_URL, static::URL_BASE, $uri);
     }
 }
